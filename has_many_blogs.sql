477e4c40e25660eb1956e3580a85fee1c77e553e
Edward Gao <egao@sent.com>
2017-01-25 18:37:52 -1000

    creates users table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -5,3 +5,13 @@ CREATE USER "has_many_user";
 DROP DATABASE IF EXISTS "has_many_blogs";
 CREATE DATABASE "has_many_blogs" OWNER "has_many_user";
 
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
+	id serial NOT NULL PRIMARY KEY,
+	username VARCHAR(90) NOT NULL,
+	first_name VARCHAR(90) NULL,
+	last_name	VARCHAR(90) NULL,
+	created_at timestamp NOT NULL now(),
+	updated_at timestamp NOT NULL now()
+);
+
