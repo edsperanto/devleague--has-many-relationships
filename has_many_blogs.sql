10f54938b69ca1cb8a606b965b40871fb11c323a
Edward Gao <egao@sent.com>
2017-01-25 18:37:18 -1000

    creates database

---
+++ has_many_blogs.sql
@@ -0,0 +1,7 @@
+-- has_many_blogs.sql
+
+DROP USER IF EXISTS "has_many_user";
+CREATE USER "has_many_user";
+DROP DATABASE IF EXISTS "has_many_blogs";
+CREATE DATABASE "has_many_blogs" OWNER "has_many_user";
+
