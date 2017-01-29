4cebd9171d1c7baddae083e8d3cc10c6e7c15699
Edward Gao <egao@sent.com>
2017-01-28 14:20:21 -1000

    number 3 and 4 works

--- joins.sql
+++ joins.sql
@@ -1,12 +1,15 @@
 -- 1.Create a query to get all fields from the users table
-
+SELECT * FROM "users";
 -- 2.Create a query to get all fields from the posts table where the user_id is 100
-
+SELECT * FROM "posts" WHERE posts.users_id = 100;
 -- 3.Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
-
-
+SELECT posts.*, users.first_name, users.last_name FROM "users" INNER JOIN "posts" ON users.id = posts.users_id WHERE users_id = 200;
 -- 4.Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
-
+SELECT posts.*, users.username 
+FROM "posts" INNER JOIN "users" 
+ON users.id = posts.users_id 
+WHERE users.first_name = 'Norene' 
+AND users.last_name = 'Schmitt';
 -- 5.Create a query to get usernames from the users table where the user has created a post after January 1, 2015
 
 -- 6.Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
