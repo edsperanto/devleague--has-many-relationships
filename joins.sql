744eda2c7e4664fa75af39078d8e0142d2d59d0d
Edward Gao <egao@sent.com>
2017-02-02 14:05:08 -1000

    step 12 working

--- joins.sql
+++ joins.sql
@@ -56,6 +56,14 @@ INNER JOIN "comments"
 ON comments.posts_id = posts.id
 WHERE comments.body LIKE '%matrix%';
 -- 12.Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
-
+SELECT users.first_name,
+users.last_name,
+comments.body AS "comment_body"
+FROM "users" INNER JOIN "comments"
+ON comments.users_id = users.id
+INNER JOIN "posts"
+ON comments.posts_id = posts.id
+WHERE comments.body LIKE '%SSL%'
+AND posts.content LIKE '%dolorum%';
 -- 13.Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
 
